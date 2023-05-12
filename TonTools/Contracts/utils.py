d0d50737f8c04b76a9678d2ad291744d380d55a6
yungwine <cyrbatoff@gmail.com>
2023-05-12 18:26:32 +0800

    change imports names

--- TonTools/Contracts/utils.py
+++ TonTools/Contracts/utils.py
@@ -1,5 +1,5 @@
 from tvm_valuetypes import deserialize_boc
-from pytonlib.utils.tlb import Transaction, Slice
+from pytonlib.utils.tlb import Transaction as PytonlibTransaction, Slice as PytonlibSlice
 from tonsdk.utils import b64str_to_bytes
 
 
@@ -11,7 +11,7 @@ def transaction_status(tr_data: str):
         cell = deserialize_boc(bytes.fromhex(tr_data))
     else:
         cell = deserialize_boc(b64str_to_bytes(tr_data))
-    tr = Transaction(Slice(cell))
+    tr = PytonlibTransaction(PytonlibSlice(cell))
     if not(tr.description.action and tr.description.action.result_code) and \
             not(tr.description.compute_ph.type == 'tr_phase_compute_vm' and tr.description.compute_ph.exit_code):
         return True
