f9a5cd4477840f81a72743308edd476079f9ed03
yungwine <cyrbatoff@gmail.com>
2023-06-01 00:30:21 +0800

    dton client refactoring

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 
-requirements = ["tonsdk>=1.0.13", "ton>=0.26", "aiohttp>=3.8.1", "setuptools>=65.3.0", "requests>=2.28.1", "pytonlib>=0.0.46"]
+requirements = ["tonsdk>=1.0.13", "ton>=0.26", "aiohttp>=3.8.1", "setuptools>=65.3.0", "requests>=2.28.1", "pytonlib>=0.0.46", "graphql-query==1.0.3"]
 
 setup(
     name='TonTools',
